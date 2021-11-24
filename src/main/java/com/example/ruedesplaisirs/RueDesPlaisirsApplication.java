1721839a45e08c5b007d3d7a1a634c4d4224879f
LenaPancher <lenapancher@gmail.com>
2021-11-24 09:03:20 +0100

    first commit

---
+++ src/main/java/com/example/ruedesplaisirs/RueDesPlaisirsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ruedesplaisirs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueDesPlaisirsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RueDesPlaisirsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ruedesplaisirs/RueDesPlaisirsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ruedesplaisirs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RueDesPlaisirsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
